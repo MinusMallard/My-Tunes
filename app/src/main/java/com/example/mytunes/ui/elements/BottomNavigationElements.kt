c7380a3630a46e525396abdd8663bf80ea760fdd
MinusMallard <rishavkumar200423@gmail.com>
2024-05-01 19:56:43 +0530

    added BottomNavigationItem for bottom navigation scaffold

---
+++ app/src/main/java/com/example/mytunes/ui/elements/BottomNavigationElements.kt
@@ -0,0 +1,40 @@
+package com.example.mytunes.ui.elements
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.List
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.ui.graphics.vector.ImageVector
+
+//data class for bottom navigation icons
+data class BottomNavigationItem (
+    val  title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val route: String
+)
+
+// defines the list of icons that are on bottom navigation bar
+val items = listOf(
+    BottomNavigationItem (
+        title = "home",
+        selectedIcon = Icons.Filled.Home,
+        unselectedIcon = Icons.Outlined.Home,
+        route = "home"
+    ),
+    BottomNavigationItem(
+        title = "Search",
+        selectedIcon = Icons.Filled.Search,
+        unselectedIcon = Icons.Outlined.Search,
+        route = "explore",
+    ),
+    BottomNavigationItem(
+        title = "",
+        selectedIcon = Icons.Filled.List,
+        unselectedIcon = Icons.Outlined.List,
+        route = "library"
+    )
+)
