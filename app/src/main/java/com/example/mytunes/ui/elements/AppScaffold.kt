39a53419ef8f0ab8c2613e968dad8ee0deb08d24
MinusMallard <rishavkumar200423@gmail.com>
2024-05-02 23:01:59 +0530

    added BottomNavigationItem for bottom navigation scaffold

--- app/src/main/java/com/example/mytunes/ui/elements/AppScaffold.kt
+++ app/src/main/java/com/example/mytunes/ui/elements/AppScaffold.kt
@@ -1,8 +1,22 @@
 package com.example.mytunes.ui.elements
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
 
-@Composable
-fun AppScaffold() {
-    
-}
