fba2d52f69eeca57a22ac23e76e1c51536c4ba5e
MinusMallard <rishavkumar200423@gmail.com>
2024-05-22 23:58:32 +0530

    added global bottom navBar

--- app/src/main/java/com/example/mytunes/navigation/NavGraph.kt
+++ app/src/main/java/com/example/mytunes/navigation/NavGraph.kt
@@ -7,16 +7,32 @@ import androidx.annotation.RequiresExtension
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import com.example.mytunes.AppViewModelProvider
+import com.example.mytunes.ui.elements.items
+import com.example.mytunes.ui.screen.GetLanguages
 import com.example.mytunes.ui.screen.HomeScreen
 import com.example.mytunes.ui.screen.GetName
 import com.example.mytunes.ui.screen.SplashScreen
@@ -33,52 +49,83 @@ fun AppNavHost(
     navController: NavHostController = rememberNavController(),
     @SuppressLint("ModifierParameter") modifier: Modifier = Modifier,
     sharedPre: SharedPreferences,
+    sharedPre2: SharedPreferences
 ) {
     val currentTime = rememberSaveable { mutableStateOf(getCurrentTime()) }
     val greeting = rememberSaveable { mutableStateOf(getGreeting(currentTime.value.toString())) }
 
-    NavHost (
-        navController = navController,
-        startDestination = "splashScreen",
-        modifier = modifier
-    ) {
 
-        composable(
-            route = "splashScreen",
-            enterTransition = { fadeIn(animationSpec = tween(500)) },
-            exitTransition = { fadeOut(animationSpec = tween(500)) }
-        ) {
-            SplashScreen(openAndPopUp = { route, popUp->
-                navController.navigate(route) {
-                    launchSingleTop = true
-                    popUpTo(popUp) { inclusive = true}
+    var selectedIconIndex by rememberSaveable {
+        mutableIntStateOf(0)
+    }
+    Scaffold(
+        bottomBar = {
+            NavigationBar(
+                containerColor = MaterialTheme.colorScheme.surface,
+                contentColor = MaterialTheme.colorScheme.onSurface,
+                modifier = Modifier.height(65.dp)
+            ) {
+                items.forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        modifier = modifier,
+                        selected = selectedIconIndex == index,
+                        onClick = {
+                            if (selectedIconIndex != index)navController.navigate(item.route){
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+
+                            selectedIconIndex = index },
+                        icon = {
+                            Icon(
+                                imageVector = if (selectedIconIndex == index) item.selectedIcon
+                                else item.unselectedIcon,
+                                contentDescription = "null")
+                        }
+                    )
                 }
-            })
-        }
-        composable(
-            route = "home",
-            enterTransition = { fadeIn(animationSpec = tween(500)) },
-            exitTransition = { fadeOut(animationSpec = tween(500)) }
-        ) {
-            val homeViewModel: HomeViewModel = viewModel(factory = AppViewModelProvider.Factory)
-            HomeScreen(
-                isLoaded = homeViewModel.isLoaded,
-                navController = navController,
-                greeting = greeting.value,
-                name = sharedPre.getString("name", "")!!,
-                searchPlaylist = {homeViewModel.getPlayList(it)},
-//                songs = homeViewModel.playListLoadState
-                )
-        }
-        composable(
-            route = "getName",
-            enterTransition = { fadeIn(animationSpec = tween(500)) },
-            exitTransition = { fadeOut(animationSpec = tween(500)) }
-        ) {
-            GetName(sharedPref = sharedPre, navigateHome = {navController.navigate("home"){
-                popUpTo("getName") {inclusive = true}
             }
-            })
+        }
+    ) {innerPadding ->
+        NavHost (
+            navController = navController,
+            startDestination = "splashScreen",
+            route = Graph.MAIN,
+            modifier = modifier.padding(innerPadding)
+        ) {
+            composable(
+                route = "splashScreen",
+                enterTransition = { fadeIn(animationSpec = tween(500)) },
+                exitTransition = { fadeOut(animationSpec = tween(500)) }
+            ) {
+                SplashScreen(openAndPopUp = { route, popUp->
+                    navController.navigate(route) {
+                        launchSingleTop = true
+                        popUpTo(popUp) { inclusive = true}
+                    }
+                })
+            }
+            composable(
+                route = "home",
+                enterTransition = { fadeIn(animationSpec = tween(500)) },
+                exitTransition = { fadeOut(animationSpec = tween(500)) }
+            ) {
+                val homeViewModel: HomeViewModel = viewModel(factory = AppViewModelProvider.Factory)
+                HomeScreen(
+                    navController = navController,
+                    greeting = greeting.value,
+                    name = sharedPre.getString("name", "")!!,
+                    trendingNow = homeViewModel.trendingNow,
+                    internationalCharts = homeViewModel.internationalCharts,
+                    edmHot40 = homeViewModel.edmHot40,
+                    romanticTop = homeViewModel.romanticTop40,
+                    homePageData = homeViewModel.homePageData
+                )
+            }
+
         }
     }
 }
@@ -93,5 +140,62 @@ private fun getGreeting(time: String): String {
         in 6..11 -> "Good morning"
         in 12..17 -> "Good afternoon"
         else -> "Good evening"
+    }
+}
+
+object Graph {
+    const val ROOT = "root"
+    const val NAME = "name"
+    const val MAIN = "main_graph"
+}
+
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+@Composable
+fun RootNavigationGraph(
+    navHostController: NavHostController = rememberNavController(),
+    sharedPre: SharedPreferences,
+    sharedPref2: SharedPreferences
+) {
+    NavHost(
+        navController = navHostController,
+        route = Graph.ROOT,
+        startDestination = Graph.NAME
+    ) {
+        val name = sharedPre.getString("name", "").toString()
+        val language = sharedPref2.getString("languages", "").toString()
+        authNavGraph(navHostController, sharedPre, sharedPref2)
+        composable(route = Graph.MAIN) {
+            AppNavHost(sharedPre = sharedPre, sharedPre2 = sharedPref2)
+        }
+    }
+}
+
+
+fun NavGraphBuilder.authNavGraph (navController: NavHostController, sharedPre: SharedPreferences, sharedPre2: SharedPreferences) {
+    navigation(
+        route = Graph.NAME,
+        startDestination = "getName"
+    ) {
+        composable(
+            route = "getName",
+            enterTransition = { fadeIn(animationSpec = tween(500)) },
+            exitTransition = { fadeOut(animationSpec = tween(500)) }
+        ) {
+            GetName(
+                sharedPref = sharedPre,
+                navigateGetLang = {navController.navigate("getLanguages"){ popUpTo("getName") {inclusive = true} } }
+            )
+        }
+
+        composable(
+            route = "getLanguages",
+            enterTransition = { fadeIn(animationSpec = tween(durationMillis = 500))},
+            exitTransition = { fadeOut(animationSpec = tween(durationMillis = 50))}
+        ) {
+            GetLanguages(
+                sharedPreference = sharedPre2,
+                navigateHome = {navController.navigate(Graph.MAIN) { popUpTo("getLanguage") {inclusive = true} } }
+            )
+        }
     }
 }
