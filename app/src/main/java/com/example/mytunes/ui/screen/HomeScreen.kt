0a73f020dd1f392c927736c9ba1c26b3de78d0e2
MinusMallard <rishavkumar200423@gmail.com>
2024-06-03 14:57:23 +0530

    added some improvements

--- app/src/main/java/com/example/mytunes/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/mytunes/ui/screen/HomeScreen.kt
@@ -1,11 +1,15 @@
 package com.example.mytunes.ui.screen
 
+import android.util.Log
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
@@ -13,103 +17,233 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.media3.common.MediaItem
+import androidx.media3.session.MediaController
 import androidx.navigation.NavHostController
-import com.example.mytunes.model.Song
-import com.example.mytunes.ui.elements.HorizontalPlaylistSong
-import com.example.mytunes.ui.elements.items
+import com.example.mytunes.ui.elements.HorizontalAlbums
+import com.example.mytunes.ui.elements.HorizontalSongBanner
+import com.example.mytunes.ui.viewModel.HomePageLoadState
+import com.example.mytunes.ui.viewModel.PlayListLoadState
+import com.google.common.util.concurrent.ListenableFuture
+import com.google.common.util.concurrent.MoreExecutors
+import java.util.concurrent.Future
 
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
+    isLoaded: Boolean = false,
     navController: NavHostController,
     greeting: String,
     name: String,
-    searchPlaylist:(String) -> Unit,
-
+    trendingNow: PlayListLoadState,
+    internationalCharts: PlayListLoadState,
+    edmHot40: PlayListLoadState,
+    romanticTop: PlayListLoadState,
+    homePageData: HomePageLoadState,
+    controllerFuture: ListenableFuture<MediaController>
 ) {
-    var selectedIconIndex by rememberSaveable {
-        mutableIntStateOf(0)
+    var index by rememberSaveable {
+        mutableStateOf<Int?>(null)
     }
-    Scaffold(
-        bottomBar = {
-            NavigationBar(
-                containerColor = MaterialTheme.colorScheme.surface,
-                contentColor = MaterialTheme.colorScheme.onSurface,
-                modifier = Modifier.height(65.dp)
-            ) {
-                items.forEachIndexed { index, item ->
-                    NavigationBarItem(
-                        modifier = modifier,
-                        selected = selectedIconIndex == index,
-                        onClick = {
-                            if (selectedIconIndex != index)navController.navigate(item.route)
-                            selectedIconIndex = index },
-                        icon = {
-                            Icon(
-                                imageVector = if (selectedIconIndex == index) item.selectedIcon
-                                else item.unselectedIcon,
-                                contentDescription = "null")
-                        }
-                    )
-                }
-            }
-        }
-    ){innerPadding ->
+    Box(modifier = Modifier.fillMaxSize()) {
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding)
+                .background(MaterialTheme.colorScheme.background)
+                .animateContentSize()
         ) {
             item {
-                Row(
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    modifier = modifier
-                        .padding(16.dp)
-                        .fillMaxWidth()
-                ) {
-                    Column{
-                        Text(
-                            text = "Hey, $name",
-                            fontWeight = FontWeight.Thin,
-                            fontSize = 12.sp
-                        )
-                        Text(
-                            text = greeting,
-                            fontWeight = FontWeight.SemiBold,
-                            fontSize = 24.sp,
-                        )
-                    }
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = Icons.Filled.Settings,
-                            contentDescription = "settings"
-                        )
-                    }
-                }
-
+                Greetings(name = name, greeting = greeting, modifier = modifier)
             }
             item {
-                val link: String = "https://www.jiosaavn.com/featured/hindi-india-superhits-top-50/zlJfJYVuyjpxWb5,FqsjKg__"
-                LaunchedEffect(Unit) {
-                    searchPlaylist(link)
+                when (trendingNow) {
+                    is PlayListLoadState.Success -> HorizontalSongBanner(
+                        songList = trendingNow.songs,
+                        name = trendingNow.name,
+                        controllerFuture = controllerFuture,
+                        onSongClick = {
+                            index = it
+                            var mediaItems: MutableList<MediaItem> = mutableListOf()
+                            controllerFuture.addListener(
+                                {
+                                    trendingNow.songs.forEach{ it ->
+                                        mediaItems.add(MediaItem.fromUri(it.downloadUrl[2].url))
+                                    }
+                                    val a = controllerFuture.get()
+                                    a.setMediaItems(mediaItems, index!!, 0)
+                                    a.prepare()
+                                    a.play()
+                                },
+                                MoreExecutors.directExecutor()
+                            )
+                        }
+                    )
+
+                    is PlayListLoadState.Loading -> {
+                        LoadingScreen()
+                    }
+
+                    is PlayListLoadState.Error -> {
+                        Log.d("MainActivity", "error2")
+                    }
+                }
+                var halfSize = 0
+                when (homePageData) {
+                    is HomePageLoadState.Success -> {
+                        halfSize = homePageData.data.data.albums.size / 2
+                        HorizontalAlbums(
+                            albums = homePageData.data.data.albums.subList(
+                                0,
+                                halfSize
+                            ),
+                            name = "Recommended Albums",
+                            controllerFuture = controllerFuture
+                        )
+                        Log.d("home", homePageData.data.data.albums.subList(0, halfSize).toString())
+                    }
+                    else -> {}
+                }
+                when (edmHot40) {
+                    is PlayListLoadState.Success -> HorizontalSongBanner(
+                        songList = edmHot40.songs,
+                        name = edmHot40.name,
+                        controllerFuture = controllerFuture,
+                        onSongClick = {
+                            index = it
+                            var mediaItems: MutableList<MediaItem> = mutableListOf()
+                            controllerFuture.addListener(
+                                {
+                                    edmHot40.songs.forEach{ it ->
+                                        mediaItems.add(MediaItem.fromUri(it.downloadUrl[2].url))
+                                    }
+                                    val a = controllerFuture.get()
+                                    a.setMediaItems(mediaItems, index!!, 0)
+                                    a.prepare()
+                                    a.play()
+                                },
+                                MoreExecutors.directExecutor()
+                            )
+                        }
+                    )
+
+                    is PlayListLoadState.Loading -> LoadingScreen()
+                    is PlayListLoadState.Error -> {}
                 }
 
+                when (homePageData) {
+                    is HomePageLoadState.Success -> {
+                        var size = homePageData.data.data.albums.size
+                        Log.d("HOME", size.toString())
+                        HorizontalAlbums(
+                            albums = homePageData.data.data.albums.subList(
+                                halfSize,
+                                size
+                            ),
+                            name = "Recommended",
+                            controllerFuture = controllerFuture
+                        )
+                    }
+
+                    is HomePageLoadState.Loading -> {
+                        Log.d("MainActivity", "This is a debug log message1")
+                    }
+
+                    is HomePageLoadState.Error1 -> {
+                        Log.d("MainActivity", "This is a debug log message2")
+                    }
+
+                    is HomePageLoadState.Error2 -> {
+                        Log.d("MainActivity", "This is a debug log message3")
+                    }
+
+                    is HomePageLoadState.Error3 -> {
+                        Log.d("MainActivity", "This is a debug log message4")
+                    }
+
+                    is HomePageLoadState.Error4 -> {
+                        Log.d("MainActivity", "This is a debug log message5")
+                    }
+                }
+                Log.d("index", index.toString())
+                when (romanticTop) {
+                    is PlayListLoadState.Success -> HorizontalSongBanner(
+                        songList = romanticTop.songs,
+                        name = romanticTop.name,
+                        controllerFuture = controllerFuture,
+                        onSongClick = {
+                            index = it
+                            var mediaItems: MutableList<MediaItem> = mutableListOf()
+                            controllerFuture.addListener(
+                                {
+                                    romanticTop.songs.forEach{ it ->
+                                        mediaItems.add(MediaItem.fromUri(it.downloadUrl[2].url))
+                                    }
+                                    val a = controllerFuture.get()
+                                    a.setMediaItems(mediaItems, index!!, 0)
+                                    a.prepare()
+                                    a.play()
+                                },
+                                MoreExecutors.directExecutor()
+                            )
+                        }
+                    )
+
+                    is PlayListLoadState.Loading -> LoadingScreen()
+                    is PlayListLoadState.Error -> {}
+                }
             }
         }
     }
+
 }
+
+@Composable
+fun Greetings(
+    name: String,
+    greeting: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier
+            .padding(start = 4.dp, bottom = 16.dp, top = 16.dp)
+            .fillMaxWidth()
+    ) {
+        Column{
+            Text(
+                text = "Hey, $name",
+                fontWeight = FontWeight.Thin,
+                fontSize = 12.sp
+            )
+            Text(
+                text = greeting,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 24.sp,
+            )
+        }
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+                imageVector = Icons.Filled.Settings,
+                contentDescription = "settings"
+            )
+        }
+    }
+}
+
+@Composable
+fun LoadingScreen() {}
+
