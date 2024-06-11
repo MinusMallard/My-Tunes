ff42a63a39469128d49a94273c012f124b3a6e6b
MinusMallard <rishavkumar200423@gmail.com>
2024-06-11 19:45:49 +0530

    added some improvements

--- app/src/main/java/com/example/mytunes/ui/elements/AlbumAndPlaylistCard.kt
+++ app/src/main/java/com/example/mytunes/ui/elements/AlbumAndPlaylistCard.kt
@@ -2,6 +2,7 @@ package com.example.mytunes.ui.elements
 
 import android.util.Log
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
@@ -9,8 +10,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -21,15 +22,18 @@ import com.example.mytunes.model.Playlist
 @Composable
 fun AlbumCard(
     album: Album,
-    modifier: Modifier = Modifier,
     navigateTo: (String) -> Unit
 ) {
+    val interactionSource = remember { MutableInteractionSource()}
     Box(
         modifier = Modifier
             .width(200.dp)
-            .clickable {
+            .clickable ( onClick = {
                 navigateTo("album/${album.id}")
-            }
+            },
+                interactionSource = interactionSource,
+                indication = null
+            )
             .padding(8.dp)
     ) {
         Column {
@@ -54,13 +58,23 @@ fun AlbumCard(
 @Composable
 fun PlaylistCard(
     album: Playlist,
-    modifier: Modifier = Modifier
+    navigateTo: (String) -> Unit
 ) {
-    Box(modifier = Modifier.width(200.dp)) {
+    val interactionSource = remember{ MutableInteractionSource()}
+    Box(
+        modifier = Modifier.width(200.dp)
+            .clickable ( onClick = {
+                    navigateTo("playlist/${album.id}")
+                },
+                interactionSource = interactionSource,
+                indication = null
+            )
+            .padding(8.dp)
+    ) {
         Column {
             Log.d("MainActivity", album.image[2].url.toString())
             CoverImage(photo = album.image[2].url.toString(), modifier = Modifier
-                .size(200.dp)
+                .size(180.dp)
                 .padding(start = 8.dp,top = 4.dp, end = 8.dp, bottom = 8.dp)
             )
             album.name?.let {
