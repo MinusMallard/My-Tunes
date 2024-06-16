b35870de5231ab03e40f716b878a838a1d523138
MinusMallard <rishavkumar200423@gmail.com>
2024-06-17 00:08:35 +0530

    fixed scaling issues

--- app/src/main/java/com/example/mytunes/ui/elements/CoverImage.kt
+++ app/src/main/java/com/example/mytunes/ui/elements/CoverImage.kt
@@ -1,26 +1,31 @@
 package com.example.mytunes.ui.elements
 
-import android.media.ImageReader
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.mytunes.R
 
 @Composable
-fun CoverImage(photo: String, modifier: Modifier = Modifier) {
+fun CoverImage(
+    photo: String,
+    modifier: Modifier = Modifier
+) {
     AsyncImage(model = ImageRequest.Builder(context = LocalContext.current)
         .data(photo)
         .crossfade(true)
         .build(),
+
         contentDescription = "image",
         contentScale = ContentScale.Crop,
-        modifier = modifier,
+        modifier = modifier
+            .clip(RoundedCornerShape(4.dp)),
         error = painterResource(R.drawable.download),
-        )
+    )
 }
