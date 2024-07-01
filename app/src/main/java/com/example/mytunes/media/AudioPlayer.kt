7fb1c0292f6973e01f7c1f54c3b15a8f37952041
MinusMallard <rishavkumar200423@gmail.com>
2024-07-01 23:48:26 +0530

    added onDestroy function

--- app/src/main/java/com/example/mytunes/media/AudioPlayer.kt
+++ app/src/main/java/com/example/mytunes/media/AudioPlayer.kt
@@ -1,66 +1,11 @@
 package com.example.mytunes.media
 
 import android.content.Intent
-import androidx.media3.common.MediaItem
-import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
 
 class PlaybackService : MediaSessionService()  {
-
-//    private lateinit var player: ExoPlayer
-//    private var mediaSession: MediaSession? = null
-//
-//
-//    override fun onCreate() {
-//        super.onCreate()
-//        val player = ExoPlayer
-//            .Builder(this)
-//            .setHandleAudioBecomingNoisy(true)
-//            .build()
-//        val mediaItem = MediaItem.fromUri("https://aac.saavncdn.com/322/935ec81cd6eaccf69bec839dc72b0802_320.mp4")
-//        player.setMediaItem(mediaItem)
-//        player.prepare()
-//        mediaSession = MediaSession.Builder(this, player).build()
-//    }
-//
-//    fun play() {
-//        player.play()
-//    }
-//
-//    fun pause() {
-//        player.pause()
-//    }
-//
-//    fun seekTo(time: Long) {
-//        player.seekTo(time)
-//    }
-//
-//    fun seekToNextMediaItem() {
-//        player.seekToNextMediaItem()
-//    }
-//
-//    fun seekToPreviousMediaItem() {
-//        player.seekToPreviousMediaItem()
-//    }
-//
-//
-//
-//    override fun onDestroy() {
-//        mediaSession?.run {
-//            player.release()
-//            release()
-//            mediaSession = null
-//        }
-//        super.onDestroy()
-//    }
-//
-//    override fun onGetSession(
-//        controllerInfo: MediaSession.ControllerInfo
-//    ): MediaSession? = mediaSession
-
-
     private var mediaSession: MediaSession? = null
 
     override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? = mediaSession
