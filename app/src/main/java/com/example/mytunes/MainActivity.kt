dfbdd8b26996e399e7f95e452f32db1b0d065cfe
MinusMallard <rishavkumar200423@gmail.com>
2024-07-09 23:09:29 +0530

    removed commented code

--- app/src/main/java/com/example/mytunes/MainActivity.kt
+++ app/src/main/java/com/example/mytunes/MainActivity.kt
@@ -1,45 +1,43 @@
 package com.example.mytunes
 
-
-import android.app.Activity
 import android.content.ComponentName
 import android.content.Context
 import android.os.Build
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
 import androidx.annotation.RequiresExtension
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowCompat
-import androidx.media3.common.MediaItem
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.media3.common.Player
+import androidx.media3.common.Player.EVENT_MEDIA_ITEM_TRANSITION
+import androidx.media3.common.Player.EVENT_MEDIA_METADATA_CHANGED
+import androidx.media3.common.Player.EVENT_TIMELINE_CHANGED
+import androidx.media3.common.Player.EVENT_TRACKS_CHANGED
 import androidx.media3.session.MediaController
 import androidx.media3.session.SessionToken
 import com.example.mytunes.media.PlaybackService
 import com.example.mytunes.navigation.RootNavigationGraph
 import com.example.mytunes.ui.theme.MyTunes
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.example.mytunes.ui.theme.MyTunesTheme
+import com.example.mytunes.ui.viewModel.SongPlayerViewModel
 import com.google.common.util.concurrent.ListenableFuture
-import com.google.common.util.concurrent.MoreExecutors
-import java.util.concurrent.Future
-
+import kotlinx.coroutines.awaitCancellation
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-
     private lateinit var controllerFuture: ListenableFuture<MediaController>
 
     @RequiresApi(Build.VERSION_CODES.R)
@@ -48,19 +46,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         val sessionToken = SessionToken(this, ComponentName(this, PlaybackService::class.java))
         controllerFuture = MediaController.Builder(this, sessionToken).buildAsync()
-//        controllerFuture.addListener(
-//            {
-//                val a = controllerFuture.get()
-//                val mediaItem = MediaItem.fromUri("https://aac.saavncdn.com/322/935ec81cd6eaccf69bec839dc72b0802_320.mp4")
-//                a.setMediaItem(mediaItem)
-//                a.prepare()
-//                a.play()
-//            },
-//            MoreExecutors.directExecutor()
-
         super.onCreate(savedInstanceState)
         setContent {
-            MyTunes {
+
+            MyTunesTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
