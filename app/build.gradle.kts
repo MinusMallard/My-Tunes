a6964017d3d419e8fc4274fc8112859275bca554
MinusMallard <rishavkumar200423@gmail.com>
2024-07-09 22:49:28 +0530

    build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -113,4 +114,10 @@ dependencies {
 
     //System Ui Controller
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.30.1")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-guava:1.8.0")
+
+    implementation("com.google.accompanist:accompanist-navigation-animation:0.35.1-alpha")
+
+
 }
