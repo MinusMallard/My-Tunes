297ac34220c8a7fa8bfd503a63c77d5d84fe7996
MinusMallard <rishavkumar200423@gmail.com>
2024-07-09 23:07:18 +0530

    added multiple improvements

--- app/src/main/java/com/example/mytunes/ui/elements/HorizontalPlayListSong.kt
+++ app/src/main/java/com/example/mytunes/ui/elements/HorizontalPlayListSong.kt
@@ -2,7 +2,6 @@ package com.example.mytunes.ui.elements
 
 import android.annotation.SuppressLint
 import android.util.Log
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
@@ -21,33 +20,24 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.media3.session.MediaController
 import com.example.mytunes.model.Artist
-import com.example.mytunes.model.DownloadUrl
 import com.example.mytunes.model.Song
-import com.google.common.util.concurrent.ListenableFuture
 @Composable
 fun HorizontalSongBanner(
     songList: List<Song>,
     name :String,
-    controllerFuture: ListenableFuture<MediaController>,
     onSongClick: (List<Song>, Song)-> Unit,
 
 ) {
-    var index by rememberSaveable {
-        mutableStateOf<Int?>(null)
-    }
+
     Box(modifier = Modifier.padding(top = 8.dp, bottom = 8.dp)) {
         Column {
             Text(
@@ -57,7 +47,7 @@ fun HorizontalSongBanner(
                 fontWeight = FontWeight.Bold
             )
             LazyHorizontalGrid(
-                modifier = Modifier.heightIn(max=250.dp),
+                modifier = Modifier.heightIn(max=280.dp),
                 rows = GridCells.Fixed(5)) {
                 items(songList) { song ->
                     SongBanner(
@@ -65,11 +55,9 @@ fun HorizontalSongBanner(
                         photo = song.image[1].url,
                         songName = song.name,
                         artists = song.artists.primary,
-                        controllerFuture = controllerFuture,
                         onSongClick = {
                             onSongClick(songList, song)
                         },
-                        songUrl = song.downloadUrl[2]
                     )
                 }
             }
@@ -84,22 +72,17 @@ fun SongBanner(
     photo: String,
     songName: String,
     artists: List<Artist>,
-    controllerFuture: ListenableFuture<MediaController>,
     onSongClick:() -> Unit,
-    songUrl: DownloadUrl,
 ) {
     val allArtists = artists[0].name.replace("&amp;", "and").replace("&quot;", "'")
-    var isSongPlaying by rememberSaveable {
+    val isSongPlaying by rememberSaveable {
         mutableStateOf(false)
     }
-    var color = if (isSongPlaying) MaterialTheme.colorScheme.primary
-    else Color.White
     Log.d("color", isSongPlaying.toString())
     Row(
         modifier = modifier
             .padding(4.dp)
             .height(50.dp)
-
             .clickable(
                 onClick = {
                     onSongClick()
@@ -122,7 +105,7 @@ fun SongBanner(
                 maxLines = 2,
                 overflow = TextOverflow.Ellipsis,
                 lineHeight = 12.sp,
-                color = color
+                color = MaterialTheme.colorScheme.onBackground
             )
             Text(
                 text = allArtists,
@@ -130,7 +113,7 @@ fun SongBanner(
                 fontSize = 12.sp,
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis,
-                color = Color.Gray,
+                color = MaterialTheme.colorScheme.inverseSurface,
                 lineHeight = 12.sp
             )
         }
